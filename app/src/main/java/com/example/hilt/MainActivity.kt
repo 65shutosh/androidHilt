218c2eee4f912eeeae9766fc1824e5a46be15a98
65shutosh <65shutosh@gmail.com>
2021-09-23 11:01:07 +0530

    constructor and field injection

--- app/src/main/java/com/example/hilt/MainActivity.kt
+++ app/src/main/java/com/example/hilt/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.hilt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var animal: Animal
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        animal.speek()
+        animal.dogSpeek()
+    }
+}
+
+class Animal @Inject constructor(private val dog: Dog) {
+
+    fun speek() {
+        println(" I can speek");
+    }
+
+    fun dogSpeek() {
+        dog.speekYourlanguage()
+    }
+
+}
+
+class Dog @Inject constructor() {
+
+    fun speekYourlanguage() {
+        println("speeking : bhaooo bhao")
     }
 }
